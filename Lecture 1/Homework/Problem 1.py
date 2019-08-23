0c99920fc1c636bf775e77a8e6f5e8b79983b426
Anahit Ganjalyan <anahit@synopsys.com>
2019-08-23 14:11:45 +0400

    adding homework to Lecture 1

---
+++ Lecture 1/Homework/Problem 1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+project = "cake"
+difficulty = "5"
+ingredients = ["flour" , "butter" , "sugar", "eggs", "cocoa powder", "baking powder"]
+
+flour = 175
+butter = 175
+sugar = "100g"
+eggs = 2
+cocoa_powder = "1st"
+baking_powder = 0.5
+
+print ("apple" in ingredients)
+print ("butter" in ingredients)
+print ("eggs" in ingredients or "margarine" in ingredients)
+print ("eggs" in ingredients and "margarine" in ingredients)
+
+print ("flour type is" ,type(flour), "\n", "butter type is:", type(butter), "\n", "sugar type is:", type(sugar), "\n","eggs type is:", type(eggs), "\n", 
+       "cocoa_powder type is:" , type(cocoa_powder), "\n", "baking_powder type is:", type(baking_powder) )
+
+print("flour -", flour, "\n" ,"butter -", butter, "\n",  "sugar -", sugar, "\n", "eggs -", eggs, "\n", "cocoa_powder -", cocoa_powder, "\n", "baking_powder -", baking_powder)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
