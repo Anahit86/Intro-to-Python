cf022e4939432b46ef0608af1cf7a5b07fa817f0
Anahit Ganjalyan <anahit@synopsys.com>
2019-08-23 14:30:58 +0400

    added Problem4 to Homework

---
+++ Lecture 1/Homework/Problem 4.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+n = int(input("input number:"))
+if (n%2==0):
+    print(n , "-even")
+else:
+    print(n, "-odd")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
